754f6cbac48ffd6138d31b5c4b51b9c3996e6e53
Lukas <lukas.gittmaier@gmail.com>
2020-10-12 15:56:19 +0200

    read csv done

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,30 +1,48 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        List<List<String>> list = new ArrayList<List<String>>();
-        try(BufferedReader br = new BufferedReader(new FileReader("numbers.csv"))){
-            String line;
-            while((line = br.readLine()) != null){
-                String[] values = line.split(";");
-                list.add(Arrays.asList(values));
+        List<Integer> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("numbers.csv"))) {
+            while (br.ready()) {
+               String[] line = br.readLine().split(":");
+               for(int i = 0; i < line.length; i++){
+                   if(isNumber(line[i])){
+                        list.add(Integer.parseInt(line[i]));
+                   }
+               }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
+
+
 
         System.out.println("Divider:");
         int divider = Integer.parseInt(s.nextLine());
 
+
+
+        for(int i = 0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
     }
-}
+
+    public static boolean isNumber(String number){
+        try {
+            int i = Integer.parseInt(number);
+        }catch(NumberFormatException n){
+            return false;
+        }
+        return true;
+    }
+    }
+
