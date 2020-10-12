452ea0c804faafa0aebe1eda874fcfc2f622b432
Lukas <lukas.gittmaier@gmail.com>
2020-10-12 14:36:06 +0200

    csv addedf

---
+++ src/main/java/JavaStreamTester.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class JavaStreamTester {
+
+    private static int getCountEmptyString(List<String> strings){
+        return 0;
+    }
+
+    private static int getCountLength3(List<String> strings){
+        return 0;
+    }
+
+    private static List<String> deleteEmptyStringsUsingJava7(List<String> strings){
+        return null;
+    }
+
+    private static String getMergedString(List<String> strings, String seperator){
+        return null;
+    }
+
+    private static List<Integer> getSquares(List<Integer> numbers){
+        return null;
+    }
+
+    private static int getMax(List<Integer> numbers){
+        return 0;
+    }
+
+    private static int getMin(List<Integer> numbers){
+        return 0;
+    }
+
+    private static int getSum(){
+        return 0;
+    }
+
+    private static int getAvarage(){
+        return 0;
+    }
+}
